ee8e7306e738e4ce0739760aaf532b9b7927efb5
Martjohn2 <115731221+Martjohn2@users.noreply.github.com>
2024-06-06 08:20:00 +0000

    FnaF -Show

--- script.js
+++ script.js
@@ -1,54 +1,8 @@
-function sendMessage() {
-  var userInput = document.getElementById("user-input").value.trim();
-  if (!userInput || isBotResponding) {
-    return;
-  }
-  var chatArea = document.getElementById("chat-area");
-  var typingIndicator = document.getElementById("typing-indicator");
-
-  var userMessage = '<div class="message-container"><div class="message user-message">' + userInput + '</div></div>';
-  chatArea.innerHTML += userMessage;
-  scrollToBottom();
-
-  // Disable input and send button
-  document.getElementById("user-input").disabled = true;
-  document.querySelector(".send-button").disabled = true;
-
-  // Display typing indicator
-  typingIndicator.style.display = "inline-block";
-
-  // Send user message to backend API
-  var question = userInput;
-  var apiKey = 'j86bwkwo-8hako-12C';
-  isBotResponding = true;
-
-  axios.get('https://lianeapi.onrender.com/@unregistered/api/wengai', {
-    params: {
-      key: apiKey,
-      query: question,
+document.getElementById('movieIdInput').addEventListener('input', function() {
+    const movieId = this.value;
+    if (movieId) {
+        const movieSource = document.getElementById('movieSource');
+        movieSource.src = `Movies/${movieId}.mp4`;
+        document.getElementById('moviePlayer').load();
     }
-  }).then(response => {
-    console.log(response.data); // Log response to console
-    var aiMessage = '<div class="message-container"><div class="message ai-message">' + response.data.message + '</div></div>';
-    chatArea.innerHTML += aiMessage;
-    typingIndicator.style.display = "none"; // Hide typing indicator
-    scrollToBottom();
-    isBotResponding = false;
-
-    // Enable input and send button
-    document.getElementById("user-input").disabled = false;
-    document.querySelector(".send-button").disabled = false;
-  }).catch(error => {
-    var errorMessage = '<div class="message-container"><div class="message ai-message">Sorry, I encountered an error.</div></div>';
-    chatArea.innerHTML += errorMessage;
-    typingIndicator.style.display = "none"; // Hide typing indicator
-    scrollToBottom();
-    isBotResponding = false;
-
-    // Enable input and send button
-    document.getElementById("user-input").disabled = false;
-    document.querySelector(".send-button").disabled = false;
-  });
-
-  document.getElementById("user-input").value = ""; // Clear input field after sending message
-}
+});
